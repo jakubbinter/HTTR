a716f015ae4294085f1c1bc856213f64b141697e
Jakub Binter <jbinter06@student.gymceska.cz>
2022-06-16 18:57:46 +0200

    Added method ParseHTML() to HttrClient and HttrClient.SendRequest() now returns ful json sturcture of nodes under desired node

--- HTTR/HttrClient.cs
+++ HTTR/HttrClient.cs
@@ -27,7 +27,7 @@ namespace HTTR
                 var obj=new JObject();
                 if (request.AtributeToRetrive == "value")
                 {
-                    obj[nodes[i].OriginalName] = nodes[i].InnerHtml;
+                    obj[nodes[i].OriginalName] = ParseHTML(nodes[i].InnerHtml);
                     retrieved.Add(obj);
                 }
                 else
@@ -42,6 +42,26 @@ namespace HTTR
             
         }
 
-        
+        JToken ParseHTML(string html)
+        {
+            
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var result = new JObject();
+            for (int i = 0; i < doc.DocumentNode.ChildNodes.Count; i++)
+            {
+                var node = doc.DocumentNode.ChildNodes[i];
+                string name = node.OriginalName;
+                if (node.HasChildNodes)
+                {
+                    result[name] = ParseHTML(node.InnerHtml);         
+                }
+                else if(doc.DocumentNode.ChildNodes.Count==1)
+                {                  
+                    return node.InnerHtml;
+                }
+            }
+            return result;
+        }
     }
 }
