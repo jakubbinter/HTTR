1fd401f98dea265d0ebd0a4f734e13d8c88fde26
Jakub Binter <jbinter06@student.gymceska.cz>
2022-06-19 18:34:01 +0200

    finished issue - 3

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,10 +1,14 @@
 ﻿using HTTR;
 
-/*var client = new HttrClient("https://www.gymceska.cz/");
-var request = new HttrRequest(tagToRetrive: "div");
-request.AddCondition("title", "Základní informace šk. roku 2021/2022");*/
-var client = new HttrClient("http://itcorp.com/");
-var request = new HttrRequest(tagToRetrive: "p");
+var client = new HttrClient("https://www.gymceska.cz/");
+var mtag = new HttrTag("div");
+mtag.AddCondition("title", "Základní informace šk. roku 2021/2022");
+var stag=new HttrTag("img",true,new string[] { "src"});
+
+var request = new HttrRequest(new List<HttrTag> { mtag ,stag});
+
+/*var client = new HttrClient("http://itcorp.com/");
+var request = new HttrRequest(new HttrTag("p"));*/
 var resp = client.SendRequest(request);
 
 Console.WriteLine(resp);
---
+++ HTTR/HttrTag.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTR
+{
+    public class HttrTag
+    {
+        public string XPath 
+        {
+            get
+            {
+                string result = "//" + TagToRetrive;
+                if (conditions == null || conditions.Count == 0)
+                    return result;
+                result += "[";
+                for (int i = 0; i < conditions.Count; i++)
+                {
+                    result += $"@{conditions[i]} and ";
+                }
+                result = result.Remove(result.Length - 5);
+                result += "]";
+                return result;
+            }
+        }
+        List<string> conditions = new List<string>();
+
+        public bool IsInnerTag { get; set; }
+        public string TagToRetrive { get; private set; }
+        public List<string> AttributesToRetrive { get; private set; }
+
+        /// <summary>
+        /// Method that adds condition 
+        /// </summary>
+        /// <param name="atribute">name of html atribute you want to set contition for</param>
+        /// <param name="eaquals">value you want the atribute to have</param>
+        public void AddCondition(string atribute, string eaquals)
+        {
+            string condition = $"{atribute}='{eaquals}'";
+            conditions.Add(condition);
+        }
+        /// <summary>
+        /// Basic Constructor
+        /// </summary>
+        /// <param name="tagToRetrive">tag this object will retrieve</param>
+        /// <param name="isInnerTag">describing if this tag should get retrived 
+        /// if set to true this tag wil get ignored by the XPath and wil not be retrived
+        /// and will only serve to describe attributes you want to get if this tag will be inside of some retrived tag</param>
+        /// <param name="attributesToRetrieve">attributes you want to retriev with this tag if not set you wil only get value of the tag</param>
+        public HttrTag(string tagToRetrive,bool isInnerTag=false,string[] attributesToRetrieve = null)
+        {
+            TagToRetrive = tagToRetrive;
+            attributesToRetrieve ??= new string[0];
+            AttributesToRetrive = attributesToRetrieve.ToList();
+            IsInnerTag = isInnerTag;
+        }
+    }
+}
