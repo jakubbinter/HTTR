0f99e430b94e8a861e2b2d9f5e3619e0cc134fd3
jakubbinter <kuba.binter@gmail.com>
2022-06-29 14:41:18 +0200

    nwm

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,11 +1,11 @@
 ﻿using HTTR;
 
-var client = new HttrClient("https://www.gymceska.cz/");
-var mtag = new HttrTag("div");
-mtag.AddCondition("title", "Základní informace šk. roku 2021/2022");
-var stag=new HttrTag("img",true,new string[] { "src"});
+var client = new HttrClient("https://www.csfd.cz/film/4324-srdce-v-atlantide/prehled/");
+var mtag = new HttrTag("img", attributesToRetrieve: new string[]{"src"});
 
-var request = new HttrRequest(new List<HttrTag> { mtag ,stag});
+
+
+var request = new HttrRequest(new List<HttrTag> { mtag});
 
 /*var client = new HttrClient("http://itcorp.com/");
 var request = new HttrRequest(new HttrTag("p"));*/
